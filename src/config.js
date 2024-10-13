e78ec06d8a389ce0efcd8224fb107d4567fa8bbe
MBALVIN <miguelob18@hotmail.com>
2024-10-13 15:39:39 -0300

    upd prefix

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const environment = process.env.REACT_APP_ENVIRONMENT || 'desarrollo';
-const boApiPrefix = process.env.REACT_APP_BO_API_PREFIX || 'https://114-181-171-103-188.ngrok-free.app/api';
+const boApiPrefix = process.env.REACT_APP_BO_API_PREFIX || 'https://7d09-181-171-103-188.ngrok-free.app/api';
 const beApiPrefix = process.env.REACT_APP_BE_API_PREFIX || 'https://1d49-38-253-66-253.ngrok-free.app';
 const version = process.env.REACT_APP_VERSION || '1.0';
 
