530654435cf365ded783b0d77401353165e5b3a1
MBALVIN <miguelob18@hotmail.com>
2024-10-19 19:09:52 -0300

    add auth func

---
+++ src/helpers/authorization.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { Config } from "../config";
+
+export const isAuthenticated = async () => {
+    const token = localStorage.getItem('authToken');
+    const idTipoUsuario = localStorage.getItem('idTipoUsuario');
+    console.log("autenticando usuario:", token);
+    console.log("idTipoUsuario:", idTipoUsuario);
+    if (!token || !idTipoUsuario) return false;
+  
+    try {
+        // Send the token to the server for validation
+        const response = await axios.post(
+            `${Config.boApiPrefix}/me`, // Correct endpoint
+            JSON.stringify({ idTipoUsuario }), // Send idTipoUsuario in request body
+            {
+              headers: {
+                'Authorization': `Bearer ${token}`, // Correct header with Bearer token
+                'Content-Type': 'application/json',
+                'accept':'application/json'
+              },
+            }
+        );
+    
+        // Check the response from the backend
+        if (response.data.isValid) {
+            return true;
+        } else {
+            console.warn('Token no es valido:', response.data.message);
+            localStorage.clear(); // Clear storage if token/role is invalid
+            return false;
+        }
+
+    } catch (error) {
+        console.error('Falla inesperada al validar Token:', error);
+        localStorage.clear();  // Clear localStorage if validation fails
+    return false;
+    }
+  };
+  
+export const logout = () => {
+    // Remove the token or any user-related information from local storage
+    localStorage.removeItem('authToken');
+    localStorage.removeItem('usuarioId');
+    localStorage.removeItem('idTipoUsuario');
+};
