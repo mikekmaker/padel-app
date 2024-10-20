a1a6181548647a094422a1c7c1a68fc060b8a396
MBALVIN <miguelob18@hotmail.com>
2024-10-19 22:36:01 -0300

    menu upd

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Navbar from './components/NavBar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import ProtectedRoute from './components/ProtectedRoute';
 import Home from './components/pages/Home';
-import SignUp from './components/pages/SignUp';
-import SignIn from './components/pages/SignIn';
 import Horarios from './components/pages/Horarios';
+import SignIn from './components/pages/SignIn';
+import SignUp from './components/pages/SignUp';
+import Profile from './components/pages/Profile';
 import '@fontsource/roboto/300.css'; // Light
 import '@fontsource/roboto/400.css'; // Regular
 import '@fontsource/roboto/500.css'; // Medium
@@ -17,10 +19,13 @@ function App() {
       <Router>
           <Navbar/>
           <Routes>
-            <Route path='/'exact Component={Home} />
-            <Route path='/registrarme'exact Component={SignUp} />
-            <Route path='/voy'exact Component={SignIn} />
+            <Route path='/' exact Component={Home} />
             <Route path='/horarios'exact Component={Horarios} />
+            <Route path='/voy'exact Component={SignIn} />
+            <Route path='/registrarme'exact Component={SignUp} />
+            <Route element={<ProtectedRoute />}>
+                    <Route path="/profile" element={<Profile />} />
+            </Route>
           </Routes>
       </Router>
     </>
