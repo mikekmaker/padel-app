732cdcefb284dd12294b83155bd53dc819a917ae
MBALVIN <miguelob18@hotmail.com>
2024-10-25 12:33:40 -0300

    update env vars

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const environment = process.env.REACT_APP_ENVIRONMENT || 'desarrollo';
-const boApiPrefix = process.env.REACT_APP_BO_API_PREFIX || 'https://7d09-181-171-103-188.ngrok-free.app/api';
-const beApiPrefix = process.env.REACT_APP_BE_API_PREFIX || 'https://1d49-38-253-66-253.ngrok-free.app';
+const boApiPrefix = process.env.REACT_APP_BO_API_PREFIX || 'http://localhost:8080/api';
+const beApiPrefix = process.env.REACT_APP_BE_API_PREFIX || 'http://localhost:8181';
 const version = process.env.REACT_APP_VERSION || '1.0';
 
 export const Config = {
